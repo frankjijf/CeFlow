0cc9307107539aa8f2de43f4b780283cbe366ec7
frankjijf <frankji199197@gmail.com>
2025-06-17 20:24:57 +1000

    250617worked1

--- CE2_6_CE_EDA_Recode.py
+++ CE2_6_CE_EDA_Recode.py
@@ -1,7 +1,7 @@
-
 import os
-import numpy as np
+import numpy as np  # 确保 nan 可用
 import pandas as pd
+import glob
 
 from CE2_2_pbin_bin_cntl import bin_cntl
 from CE2_3_pnorm_nom_cntl import nom_cntl
@@ -64,6 +64,8 @@ def CE_EDA_Recode(insdn: pd.DataFrame,
             profile_df = pd.concat([profile_df, prof_bin], ignore_index=True)
     else:
         keep_B = []
+        vars2_bin = pd.DataFrame()
+        typ_map = {}
 
     # 5. Nominal variables
     if nom_vars:
@@ -83,6 +85,7 @@ def CE_EDA_Recode(insdn: pd.DataFrame,
             profile_df = pd.concat([profile_df, prof_nom], ignore_index=True)
     else:
         keep_N = []
+        vars2_nom = pd.DataFrame()
 
     # 6. Ordinal variables
     if ord_vars:
@@ -100,6 +103,7 @@ def CE_EDA_Recode(insdn: pd.DataFrame,
             profile_df = pd.concat([profile_df, prof_ord], ignore_index=True)
     else:
         keep_O = []
+        vars2_ord = pd.DataFrame()
 
     # 7. Continuous variables
     if cont_vars:
@@ -117,93 +121,65 @@ def CE_EDA_Recode(insdn: pd.DataFrame,
             profile_df = pd.concat([profile_df, prof_cont], ignore_index=True)
     else:
         keep_C = []
+        vars2_cont = pd.DataFrame()
 
     # 8. Apply recode to entire dataset (ensuring new_vars exist)
-    recoded = insdn.copy()
-    # run each cntl with profiling=False to generate new_vars on recoded
-    if bin_vars:
-        bin_cntl(
-            df=recoded,
-            bin_vars=bin_vars,
-            vars2=vars2_bin,              # reuse the one with 'label'
-            path_output=path_output,
-            prefix=prefix,
-            dep_var=dep_var,
-            missrate=missrate,
-            concrate=concrate,
-            profiling=False,              # 这里关闭 profiling
-            typ_map=typ_map               # 然后才是 typ_map
-        )
-    if nom_vars:
-        nom_cntl(
-            df=recoded,
-            nom_vars=nom_vars,
-            vars2=vars2_nom,
-            path_output=path_output,
-            prefix=prefix,
-            dep_var=dep_var,
-            missrate=missrate,
-            concrate=concrate,
-            valcnt=valcnt,
-            minbinnc=minbinnc,
-            minbinnp=minbinnp,
-            talpha=talpha,
-            bonfer=bonfer,
-            nom_method=nom_method,
-            binary_dv=(recoded[dep_var].nunique()==2)
-        )
-    if ord_vars:
-        ord_cntl(
-            df=recoded,
-            ord_vars=ord_vars,
-            vars2=vars2_ord,
-            path_output=path_output,
-            prefix=prefix,
-            dep_var=dep_var,
-            missrate=missrate,
-            concrate=concrate,
-            profiling=False,
-            transformationO=transformationO,
-            num_category=num_category
-        )
-    if cont_vars:
-        cont_cntl(
-            df=recoded,
-            cont_vars=cont_vars,
-            vars2=vars2_cont,
-            path_output=path_output,
-            prefix=prefix,
-            dep_var=dep_var,
-            missrate=missrate,
-            profiling=False,
-            transformationC=transformationC,
-            p_lo=p_lo,
-            p_hi=p_hi
-        )
+    recoded = workfile.copy()
+    for script in glob.glob(os.path.join(path_output, "CE2_*_Var_Recode.py")):
+        # 把脚本读进来
+        code = open(script, 'r').read()
+        # 在 recoded DataFrame 上执行
+        # 保证 df 名称一致，并且 np 也可用
+        local_vars = {'df': recoded, 'np': np, 'nan': np.nan}
+        exec(code, local_vars)
+        # 更新 recoded，确保新变量被加入
+        recoded = local_vars['df']
+        exec(code, {'df': recoded, 'np': np, 'nan': np.nan})
 
-    # 9. Build CE2_Recoded (keep id, dep_var, mod_val_test and all new_vars)
+    # 然后就能选出所有新变量
     base_keep = [c for c in ['rid', 'mod_val_test', dep_var] if c in recoded.columns]
-    all_new = keep_B + keep_N + keep_O + keep_C
+    all_new   = keep_B + keep_N + keep_O + keep_C
     CE2_Recoded = recoded[base_keep + all_new].copy()
 
     # 10. Correlation and Excel report
-    corr = CE2_Recoded.corr()[dep_var].drop(dep_var).abs().sort_values(ascending=False)
-    corr_df = corr.reset_index().rename(columns={'index': 'variable', dep_var: 'correlation'})
+    if dep_var in CE2_Recoded.columns and pd.api.types.is_numeric_dtype(CE2_Recoded[dep_var]):
+        corr = CE2_Recoded.corr()[dep_var].drop(dep_var).abs().sort_values(ascending=False)
+        corr_df = corr.reset_index().rename(columns={'index': 'variable', dep_var: 'correlation'})
+    else:
+        corr_df = pd.DataFrame(columns=['variable', 'correlation'])
 
     writer = pd.ExcelWriter(os.path.join(path_output, 'CE2_EDA_report.xlsx'),
                             engine='xlsxwriter')
-    if keep_B: pd.DataFrame(vars2_bin).assign(new_var=keep_B).to_excel(writer, 'Binary', index=False)
-    if keep_N: pd.DataFrame(vars2_nom).assign(new_vars=keep_N).to_excel(writer, 'Nominal', index=False)
-    if keep_O: pd.DataFrame(vars2_ord).assign(new_var=keep_O).to_excel(writer, 'Ordinal', index=False)
-    if keep_C: pd.DataFrame(vars2_cont).assign(new_var=keep_C).to_excel(writer, 'Continuous', index=False)
-    if profiling and not profile_df.empty: profile_df.to_excel(writer, 'Profile', index=False)
-    corr_df.to_excel(writer, 'Correlations', index=False)
-    writer.save()
+    if keep_B: pd.DataFrame(vars2_bin).assign(new_var=pd.Series(keep_B)).to_excel(writer, 'Binary', index=False)
+    if keep_N: pd.DataFrame(vars2_nom).assign(new_var=pd.Series(keep_N)).to_excel(writer, 'Nominal', index=False)
+    if keep_O: pd.DataFrame(vars2_ord).assign(new_var=pd.Series(keep_O)).to_excel(writer, 'Ordinal', index=False)
+    if keep_C: pd.DataFrame(vars2_cont).assign(new_var=pd.Series(keep_C)).to_excel(writer, 'Continuous', index=False)
+    writer.close()
 
     # 11. Variable lookup
     lookup = []
     for v in all_new:
-        lbl = recoded[v].attrs.get('label', '')
+        lbl = v
+        if not vars2_bin.empty and v in getattr(vars2_bin, 'name', []):
+            try:
+                lbl = vars2_bin.loc[vars2_bin['name'] == v, 'label'].values[0]
+            except Exception:
+                lbl = v
+        elif not vars2_nom.empty and v in getattr(vars2_nom, 'name', []):
+            try:
+                lbl = vars2_nom.loc[vars2_nom['name'] == v, 'label'].values[0]
+            except Exception:
+                lbl = v
+        elif not vars2_ord.empty and v in getattr(vars2_ord, 'name', []):
+            try:
+                lbl = vars2_ord.loc[vars2_ord['name'] == v, 'label'].values[0]
+            except Exception:
+                lbl = v
+        elif not vars2_cont.empty and hasattr(vars2_cont, 'name') and v in vars2_cont['name'].values:
+            try:
+                lbl = vars2_cont.loc[vars2_cont['name'] == v, 'label'].values[0]
+            except Exception:
+                lbl = v
         lookup.append((v, lbl, v, lbl))
     var_lookup_df = pd.DataFrame(lookup,
                                  columns=['variable', 'label', 'orig_var', 'orig_label'])
@@ -211,6 +187,8 @@ def CE_EDA_Recode(insdn: pd.DataFrame,
     return CE2_Recoded, profile_df, var_lookup_df
 
 # 假设 titanic 已经有 mod_val_test 列了
+# 示例数据路径为硬编码，如需复现请将数据集放在相对路径下或修改为自己的数据路径
+# 例如: df = pd.read_csv('./CE1_Resampled_Titanic.csv')
 df = pd.read_csv('D:/OneDrive/CE_PROJECT/Python_CE_Project_GH/SAS2PYTHON/CE1_Resampled_Titanic.csv')
 
 df['IsFemale'] = (df['Sex']=='female').astype(int)
--- CE2_Ordinal_Var_Recode.py
+++ CE2_Ordinal_Var_Recode.py
@@ -8,3 +8,9 @@ df['R1_Pclass'].attrs['label'] = 'Pclass: Recode (-)'
 # --- Final clip & label for Pclass ---
 df['R1_Pclass'] = df['R1_Pclass'].clip(lower=1.0, upper=3.0)
 df['R1_Pclass'].attrs['label'] = 'Pclass: Recode (-)'
+df['Pclass'] = df['R1_Pclass']
+df['Pclass'].attrs['label'] = 'Pclass Pclass (-)'
+
+
+# KEEP_LIST_O
+KEEP_LIST_O = ['Pclass']
